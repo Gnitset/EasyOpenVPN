ff1823c28aa8a25d49b4dcf50eae467592142118
Klas Meder Boqvist <github.com@servern.info>
2015-09-23 01:21:40 +0200

    Add the first version of openvpn-script.py

---
+++ openvpn-script.py
@@ -0,0 +1,182 @@
+#! /usr/bin/env python
+
+# deps on python-bcrypt python-pysqlite2
+
+import os
+import sys
+import sqlite3
+
+def manage():
+	import argparse
+	parser = argparse.ArgumentParser(description='Manage the user/access-db for openvpn')
+	parser.add_argument('-a', '--add', action='store_true')
+#	parser.add_argument('-e', '--enable', action='store_true')
+#	parser.add_argument('-d', '--disable', action='store_true')
+	parser.add_argument('-r', '--remove', action='store_true')
+	parser.add_argument('-l', '--list', action='store_true')
+	parser.add_argument('-m', '--map', action='store_true')
+	parser.add_argument('--chpass', action='store_true')
+	parser.add_argument('--initdb', action='store_true')
+	parser.add_argument('-u', '--user')
+	parser.add_argument('-n', '--network')
+	args = parser.parse_args()
+
+	if len(sys.argv) < 2:
+		parser.print_help()
+		sys.exit(1)
+
+	if args.initdb:
+		if raw_input("Really initialize db and remove all in it? (y/N) ").lower() != 'y':
+			sys.exit(1)
+		else:
+			print "OK, wiping DB"
+			init_db()
+			sys.exit(0)
+	elif args.chpass:
+		update_password(args.user)
+		print "Password changed"
+		sys.exit(0)
+
+	if args.list and not args.map:
+		if args.user:
+			list_users()
+		if args.network:
+			list_networks()
+	elif args.list and args.map:
+		pass # not implemented
+
+	if args.add:
+		if args.user and not args.network:
+			add_user(args.user)
+			update_password(args.user)
+		elif args.network and not args.user:
+			add_network(args.network)
+		elif args.map and args.user and args.network:
+			add_map(args.user, args.network)
+		else:
+			print "Add users and networks one at the time or add a map"
+	elif args.remove:
+		if args.user and not args.network:
+			remove_user(args.user, password)
+		elif args.network and not args.user:
+			remove_network(args.network)
+		elif args.map and args.user and args.network:
+			remove_map(args.user, args.network)
+		else:
+			print "Remove users or networks one at the time or remove a map"
+
+def add_user(user):
+	c.execute("INSERT INTO users (username) VALUES (?)", (user,))
+	conn.commit()
+
+def remove_user(user,password):
+	c.execute("DELETE FROM network_map WHERE username = ?", (user,))
+	c.execute("DELETE FROM users WHERE username = ?", (user,))
+	conn.commit()
+
+def list_users():
+	for (user, status) in c.execute("SELECT username, inactive FROM users"):
+		if status == 0:
+			print "%s\tACTIVE"%user
+		else:
+			print "%s\tInactive"%user
+
+def add_network(network):
+	c.execute("INSERT INTO networks (network) VALUES (?)", (network,))
+	conn.commit()
+
+def remove_network(network):
+	c.execute("DELETE FROM network_map WHERE network = ?", (network,))
+	c.execute("DELETE FROM networks WHERE network = ?", (network,))
+	conn.commit()
+
+def list_networks():
+	for (network, description) in c.execute("SELECT network, description FROM networks"):
+		print "%s\t%s"%(network, description)
+
+def add_map(user, network):
+	c.execute("INSERT INTO network_map (username, network) VALUES (?, ?)", (user, network))
+	conn.commit()
+
+def remove_map(map):
+	c.execute("DELETE FROM network_map WHERE username = ? AND network = ?", (user, network))
+	conn.commit()
+
+def list_maps():
+	for (username, network) in c.execute("SELECT username, network FROM network_map"):
+		print "%s\t%s"%(username, network)
+
+def init_db():
+	c.execute("CREATE TABLE IF NOT EXISTS users (username TEXT PRIMARY KEY, password TEXT, two_factor_id TEXT DEFAULT NULL, inactive INTEGER DEFAULT 0)")
+	c.execute("CREATE TABLE IF NOT EXISTS networks (network TEXT PRIMARY KEY CHECK ( LIKE('%/%', network) ), description TEXT)")
+	c.execute("CREATE TABLE IF NOT EXISTS network_map (username TEXT REFERENCES users(username), network TEXT REFERENCES networks(network), CONSTRAINT pk PRIMARY KEY (username, network))")
+	conn.commit()
+
+def update_password(user):
+	import bcrypt
+	import getpass
+	while True:
+		password1 = getpass.getpass("Password: ")
+		password2 = getpass.getpass("Password again: ")
+		if password1 == password2:
+			break
+		else:
+			print "Passwords didn't match, try again"
+	hashed_password = bcrypt.hashpw(password1, bcrypt.gensalt())
+	c.execute("UPDATE users SET password = ? WHERE username = ?", (hashed_password, user))
+	conn.commit()
+
+def iptables(ar, ip, net):
+	os.spawnv(os.P_WAIT, "/sbin/iptables", ["iptables", "-%s"%ar.upper(), "FORWARD", "-s", ip, "-d", net, "-j", "ACCEPT"])
+	os.spawnv(os.P_WAIT, "/sbin/iptables", ["iptables", "-%s"%ar.upper(), "FORWARD", "-s", net, "-d", ip, "-j", "ACCEPT"])
+
+def user_pass_verify():
+	import bcrypt
+	user = os.environ['username']
+	password = os.environ['password']
+	password_hash = c.execute('SELECT password FROM users WHERE username = ?', (user,)).fetchall()[0][0]
+	if bcrypt.hashpw(password, password_hash) == password_hash:
+		sys.exit(0)
+	else:
+		sys.exit(1)
+
+def client_connect():
+	networks = c.execute('SELECT network FROM network_map WHERE username = ?', (os.environ['username'],)).fetchall()
+	if networks:
+		import socket, struct
+		c_conf=open(sys.argv[1], "a+")
+		for network in networks:
+			try:
+				net,cidr = network[0].split("/",1)
+				netmask = socket.inet_ntoa(struct.pack(">I", (0xffffffff << (32 - int(cidr))) & 0xffffffff))
+				c_conf.write('push "route %s %s"\n'%(net,netmask))
+				iptables("a", os.environ['ifconfig_pool_remote_ip'], network[0])
+			except ValueError:
+				continue
+	sys.exit(0)
+
+def client_disconnect():
+	for network in c.execute('SELECT network FROM network_map WHERE username = ?', (os.environ['username'],)):
+		iptables("d", os.environ['ifconfig_pool_remote_ip'], network[0])
+	sys.exit(0)
+
+def connect_db(db_file):
+	global conn, c
+	conn = sqlite3.connect(db_file)
+	c = conn.cursor()
+	c.execute("PRAGMA foreign_keys = ON")
+	conn.commit()
+
+if __name__ == "__main__":
+	db_file = '/etc/openvpn/access.sqlite3'
+	connect_db(db_file)
+
+	if not os.environ.has_key('script_type'):
+		manage()
+		sys.exit(1)
+
+	lookup_method = { 'user-pass-verify': user_pass_verify, 'client-connect': client_connect, 'client-disconnect': client_disconnect }
+	
+	lookup_method[os.environ['script_type']]()
+
+	sys.exit(1)
