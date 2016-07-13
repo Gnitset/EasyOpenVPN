03633335c12050017b69f499079224571d95a989
Klas Meder Boqvist <github.com@servern.info>
2016-07-13 20:09:13 +0200

    Break out sqlite backend to module

--- openvpn_script.py
+++ openvpn_script.py
@@ -1,81 +1,14 @@
 #! /usr/bin/env python
 
-# deps on python-bcrypt python-pysqlite2
-
 import os
 import sys
-import sqlite3
 
 try:
-	from config import db_file
+	from config import db, db_config
+	db.connect_db(**db_config)
 except ImportError:
-	db_file = 'access.sqlite3'
-
-
-class User(object):
-	def __init__(self, username):
-		assert username != None
-		self.username = username
-
-	def use_two_factor_auth(self):
-		if c.execute("SELECT two_factor_id FROM users WHERE username = ?", (self.username,)).fetchall()[0][0]:
-			return True
-		else:
-			return False
-
-	def get_google_authenticator_secrets(self):
-		return zip(*c.execute("SELECT google_authenticator_secret FROM google_authenticator_secrets WHERE username = ? AND inactive = 0", (self.username,)).fetchall())[0]
-
-	def validate_password(self, password, only_if_active = True):
-		import bcrypt
-		if only_if_active:
-			password_hash = c.execute('SELECT password FROM users WHERE username = ? AND inactive = 0', (self.username,)).fetchall()[0][0].encode('utf-8')
-		else:
-			password_hash = c.execute('SELECT password FROM users WHERE username = ?', (self.username,)).fetchall()[0][0].encode('utf-8')
-		if bcrypt.hashpw(password, password_hash) == password_hash:
-			return True
-		else:
-			return False
-
-
-class GoogleAuthenticator(object):
-	def __init__(self, otp):
-		self.otp = otp
-
-	def set_secret_key(self, secret_key):
-		self._secret_key = secret_key
-		return True
-
-	def validate(self):
-		"""Stolen from http://www.brool.com/post/using-google-authenticator-for-your-website/"""
-		import time
-		import struct
-		import hmac
-		import hashlib
-		import base64
-
-		tm = int(time.time() / 30)
-		secret_key = base64.b32decode(self._secret_key)
-		# try 30 seconds behind and ahead as well
-		for ix in [0, -1, 1]:
-			# convert timestamp to raw bytes
-			b = struct.pack(">q", tm + ix)
-
-			# generate HMAC-SHA1 from timestamp based on secret key
-			hm = hmac.HMAC(secret_key, b, hashlib.sha1).digest()
-
-			# extract 4 bytes from digest based on LSB
-			offset = ord(hm[-1]) & 0x0F
-			truncatedHash = hm[offset:offset+4]
-
-			# get the code from it
-			code = struct.unpack(">L", truncatedHash)[0]
-			code &= 0x7FFFFFFF;
-			code %= 1000000;
-
-			if ("%06d" % code) == str(self.otp):
-				return True
-		return False
+	from db.sqlite3 import db
+	db.connect_db(db_file = "access.sqlite3")
 
 
 class DummyFirewall(object):
@@ -139,33 +72,13 @@ class Script(object):
 		getattr(self, "_%s" % script_type.replace("-", "_"))()
 
 	def _user_pass_verify(self):
-		user = User(os.environ['username'])
-		input_password = os.environ['password']
-		if user.use_two_factor_auth():
-			try:
-				ga_otp_s = input_password[-6:]
-				assert len(ga_otp_s) == 6
-				ga_otp = int(ga_otp_s)
-				ga = GoogleAuthenticator(ga_otp)
-				valid_ga_secrets = set()
-				for secret in user.get_google_authenticator_secrets():
-					if user.validate_password(input_password[:-6]) and ga.set_secret_key(secret) and ga.validate():
-						valid_ga_secrets.add(secret)
-				if len(valid_ga_secrets) == 1:
-					sys.exit(0)
-				else:
-					sys.exit(1)
-			except ValueError, ve:
-				sys.exit(1)
+		if db.validate_user_password(os.environ["username"], os.environ["password"]):
+			sys.exit(0)
 		else:
-			password = os.environ['password']
-			if user.validate_password(password):
-				sys.exit(0)
-			else:
-				sys.exit(1)
+			sys.exit(1)
 
 	def _client_connect(self):
-		networks = c.execute('SELECT network FROM network_map WHERE username = ?', (os.environ['username'],)).fetchall()
+		networks = db.get_user_networks(os.environ["username"])
 		if networks:
 			if os.geteuid() == 0 and os.path.isfile("/sbin/iptables"):
 				firewall = IpTables
@@ -175,12 +88,11 @@ class Script(object):
 				firewall = DummyFirewall
 			fw = firewall(os.environ['ifconfig_pool_remote_ip'])
 			c_conf=open(sys.argv[1], "a+")
-			for network in networks:
+			for (network, netmask) in networks:
 				try:
-					net,cidr = network[0].split("/",1)
-					netmask = Helpers.netmask_from_cidr(cidr)
-					c_conf.write('push "route %s %s"\n'%(net,netmask))
-					fw.add_rule(os.environ['ifconfig_pool_remote_ip'], network[0])
+					c_conf.write("push \"route %s %s\"\n" % (network, netmask))
+					fw.add_rule(os.environ["ifconfig_pool_remote_ip"], "%s/%s" %
+						(network, sum([bin(int(x)).count('1') for x in netmask.split('.')])))
 				except ValueError:
 					continue
 			fw.commit()
@@ -197,30 +109,10 @@ class Script(object):
 		sys.exit(0)
 
 
-class Helpers(object):
-	def __init__(self):
-		raise Exception("Only static methods here")
-
-	@staticmethod
-	def netmask_from_cidr(cidr):
-		import socket, struct
-		return socket.inet_ntoa(struct.pack(">I", (0xffffffff << (32 - int(cidr))) & 0xffffffff))
-
-	@staticmethod
-	def connect_db(db_file):
-		global conn, c
-		conn = sqlite3.connect(db_file)
-		c = conn.cursor()
-		c.execute("PRAGMA foreign_keys = ON")
-		conn.commit()
-
-
 if __name__ == "__main__":
-	Helpers.connect_db(db_file)
-
 	if os.environ.has_key('script_type'):
 		Script(os.environ['script_type'])
 	else:
-		Manage()
+		print "Not called from openvpn"
 
 	sys.exit(1)
